2d9a357c172b95b4c91e6c51744b0ddbf168c43e
Jeroen Engels <jfm.engels@gmail.com>
2016-04-14 01:35:15 +0200

    Add comments in `enhance.js`

--- rules/core/enhance.js
+++ rules/core/enhance.js
@@ -30,6 +30,9 @@ function strippedModuleName(strippedName, name) {
 module.exports = function createAvaRule() {
   var imports = {};
 
+  // `ImportDeclaration` and `VariableDeclarator` will find Lodash imports and require()
+  // and fill `imports` with what is found
+  // The result is a collection of identifier and (method or Lodash) pairs
   var predefinedRules = {
     ImportDeclaration: function (node) {
       var name = node.source.value;
@@ -37,8 +40,14 @@ module.exports = function createAvaRule() {
         var strippedName = stripLodash(name);
         node.specifiers.forEach(function (specifier) {
           if (specifier.type === 'ImportDefaultSpecifier') {
+            // `import _ from 'lodash';` --> {'_': ''}
+            // `import _ from 'lodash/fp';` --> {'_': 'fp'}
+            // `import find from 'lodash/find';` --> {'find': 'find'}
+            // `import find from 'lodash/fp/find';` --> {'find': 'fp/find'}
             imports[specifier.local.name] = strippedName;
           } else if (specifier.type === 'ImportSpecifier') {
+            // `import {find, invoke as i} from 'lodash';` --> {'find': 'find', 'i': 'invoke'}
+            // `import {find, invoke as i} from 'lodash/fp';` --> {'find': 'fp/find', 'i': 'fp/invoke'}
             imports[specifier.local.name] = strippedModuleName(strippedName, specifier.imported.name);
           }
         });
@@ -50,9 +59,15 @@ module.exports = function createAvaRule() {
         if (isLodashModule(name)) {
           var strippedName = stripLodash(name);
           if (node.id.type === 'Identifier') {
+            // `var _ = require('lodash');` --> {'_': ''}
+            // `var _ = require('lodash/fp');` --> {'_': 'fp'}
+            // `var find = require('lodash/find');` --> {'find': 'find'}
+            // `var find = require('lodash/fp/find');` --> {'find': 'fp/find'}
             imports[node.id.name] = strippedName;
           } else if (node.id.type === 'ObjectPattern') {
             node.id.properties.forEach(function (prop) {
+              // `var {find, invoke: i} = require('lodash');` --> {'find': 'find', 'i': 'invoke'}
+              // `var {find, invoke: i} = require('lodash/fp');` --> {'find': 'fp/find', 'i': 'fp/invoke'}
               imports[prop.key.name] = strippedModuleName(strippedName, prop.value.name);
             });
           }
@@ -86,17 +101,40 @@ module.exports = function createAvaRule() {
     }
   };
 
-  Object.defineProperty(rule, 'imports', {
-    get: function () {
-      return imports;
-    }
-  });
+  rule.imports = imports;
 
-  rule.is = function is(name, expected, canBeNonFp) {
+  /**
+   * Checks if the identifier `id` corresponds to Lodash or one of its modules.
+   * @param  {string}  id          The name of the identifier.
+   * @param  {string}  expected    The name of the method. Special case for 'lodash' which checks for the `lodash` object
+   * @param  {boolean}  canBeNonFp If false or undefined, will only check if method is the FP version.
+*                              		 If true, will check both FP and
+   * @example
+   * imports = {
+   *   '_': '', // vanilla lodash
+   *   'F': 'fp', // FP lodash
+   *   'vFind': 'find', // vanilla find
+   *   'fpFind': 'fp/find', // FP find
+   * }
+   *
+   * rule.is('_', 'lodash') // => false
+   * rule.is('_', 'lodash', true) // => true
+   * rule.is('F', 'lodash') // => true
+   * rule.is('F', 'lodash', true) // => true
+   *
+   * rule.is('vFind', 'find') // => false
+   * rule.is('vFind', 'find', true) // => true
+   * rule.is('fpFind', 'find') // => true
+   * rule.is('fpFind', 'find', true) // => true
+   *
+   * @return {Boolean}            True if the id matches the expected method.
+   */
+  rule.is = function is(id, expected, canBeNonFp) {
+    var imp = imports[id];
     if (expected === 'lodash') {
-      return imports[name] === 'fp' || (canBeNonFp && imports[name] === '');
+      return imp === 'fp' || (canBeNonFp && imp === '');
     }
-    return imports[name] === 'fp/' + expected || (canBeNonFp && imports[name] === expected);
+    return imp === 'fp/' + expected || (canBeNonFp && imp === expected);
   };
 
   return rule;
