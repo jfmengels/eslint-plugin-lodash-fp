53c7c7d0d838e52f14970d04165d2bbb2027ef01
Jeroen Engels <jfm.engels@gmail.com>
2016-05-01 22:48:06 +0200

    Improve `use-fp`

--- rules/use-fp.js
+++ rules/use-fp.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var enhance = require('./core/enhance');
+var isStaticRequire = require('./core/static-require');
 
 function reportIfLodashButNotFp(context, node, name) {
   if (name && name.indexOf('lodash') === 0 && name.indexOf('lodash/fp') === -1) {
@@ -16,7 +17,7 @@ module.exports = function (context) {
       reportIfLodashButNotFp(context, node, node.source.value);
     },
     CallExpression: function (node) {
-      if (node.callee.name === 'require' && node.arguments.length > 0) {
+      if (isStaticRequire(node)) {
         reportIfLodashButNotFp(context, node, node.arguments[0].value);
       }
     }
