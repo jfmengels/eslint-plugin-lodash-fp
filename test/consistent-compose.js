c5268b041b25e44fffd47825a1517f05330380dd
Jeroen Engels <jfm.engels@gmail.com>
2016-05-15 17:51:17 +0200

    Comment nit [ci skip]

--- test/consistent-compose.js
+++ test/consistent-compose.js
@@ -83,7 +83,7 @@ test(() => {
         code: code(`import {map as flow} from 'lodash/fp'; flow(fn1, iterable);`, false),
         options: ['compose']
       },
-      // Should not warn if no compose function name specified
+      // Should not warn if no compose function is specified
       {
         code: code(`compose(fn1, fn2); pipe(fn1, fn2);`, ['compose', 'pipe']),
         options: []
