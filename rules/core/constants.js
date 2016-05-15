bd74e90264fe484b1bfae7defa14ac75269d77bc
kav singh <flakisms@gmail.com>
2016-05-15 11:28:30 +1000

    no-single-composition use util/isComposeMethod function for consistency

--- rules/core/constants.js
+++ rules/core/constants.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var COMPOSITION_METHODS = ['flow', 'flowRight', 'pipe', 'compose'];
+var COMPOSITION_METHODS = ['compose', 'flow', 'flowRight', 'pipe'];
 
 module.exports = {
   COMPOSITION_METHODS: COMPOSITION_METHODS
