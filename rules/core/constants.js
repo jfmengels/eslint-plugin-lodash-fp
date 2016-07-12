d759e517dd5611d247bc0bd97c8e87712185ca0b
Dustin Fadler <dfadler@apple.com>
2016-07-12 13:39:00 -0700

    Adds bindAll to methods excluded from no-unused-result

--- rules/core/constants.js
+++ rules/core/constants.js
@@ -2,8 +2,10 @@
 
 const COMPOSITION_METHODS = ['compose', 'flow', 'flowRight', 'pipe'];
 const FOREACH_METHODS = ['forEach', 'forEachRight', 'each', 'eachRight', 'forIn', 'forInRight', 'forOwn', 'forOwnRight'];
+const SIDE_EFFECT_METHODS = FOREACH_METHODS.concat(['bindAll']);
 
 module.exports = {
   COMPOSITION_METHODS,
-  FOREACH_METHODS
+  FOREACH_METHODS,
+  SIDE_EFFECT_METHODS
 };
