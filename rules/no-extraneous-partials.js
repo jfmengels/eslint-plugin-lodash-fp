10aad9f9398d4d514ec8a2292d76a0e0c34fd486
Benjamin Vincent <luxcium@neb401.com>
2020-05-08 05:24:29 -0400

    fix: lint issue
    284
      rules/no-extraneous-partials.js:6:53
    285
      ✖   6:53  There should be no space after {.                     object-curly-spacing
    286
      ✖   6:77  There should be no space before }.                    object-curly-spacing
    287
      ✖  13:13  There should be no space after {.                     object-curly-spacing
    288
      ✖  13:22  There should be no space before }.                    object-curly-spacing
    289
      ✖  29:7   Comments should not begin with a lowercase character  capitalized-comments
    290
    291

--- rules/no-extraneous-partials.js
+++ rules/no-extraneous-partials.js
@@ -3,14 +3,14 @@
 const _ = require('lodash/fp');
 const enhance = require('./core/enhance');
 
-const hasSpread = _.flow(_.get('arguments'), _.some({ type: 'SpreadElement' }));
+const hasSpread = _.flow(_.get('arguments'), _.some({type: 'SpreadElement'}));
 
 const create = function (context) {
   const info = enhance();
 
   return info.merge({
     CallExpression(node) {
-      const { callee } = node;
+      const {callee} = node;
       const method = info.helpers.isMethodCall(callee);
       if (method && !method.skipFixed && (callee.arguments.length || 1) < method.ary && !hasSpread(callee)) {
         context.report(node, `\`${method.name}\` should be called without an intermediate partial.`);
@@ -26,7 +26,6 @@ module.exports = {
       description: 'Avoid unnecessary intermediate partials in curried methods.',
       recommended: 'off',
 
-      // no-extraneous-partials.js
       url: 'https://github.com/jfmengels/eslint-plugin-lodash-fp/blob/master/docs/rules/no-extraneous-partials.md'
     }
   }
